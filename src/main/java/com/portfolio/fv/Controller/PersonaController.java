4ef6d520a484ad6a7e40123ef776411e41040609
fernandodebsas <fernandodebsas@hotmail.com>
2022-05-29 19:17:06 -0300

    edited cors origins

--- src/main/java/com/portfolio/fv/Controller/PersonaController.java
+++ src/main/java/com/portfolio/fv/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://portfoliofv.firebaseapp.com/iniciar-sesion")
+@CrossOrigin(origins = {"http://localhost:4200", "https://portfoliofv.web.app/iniciar-sesion" })
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
