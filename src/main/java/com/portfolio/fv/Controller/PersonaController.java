de819e2a1b346852a893ce950283e4fd02bdf10d
fernandodebsas <fernandodebsas@hotmail.com>
2022-05-29 21:54:40 -0300

    @CrossOrigin is back

--- src/main/java/com/portfolio/fv/Controller/PersonaController.java
+++ src/main/java/com/portfolio/fv/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.fv.Entity.Persona;
 import com.portfolio.fv.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://portfoliofv.firebaseapp.com/")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
