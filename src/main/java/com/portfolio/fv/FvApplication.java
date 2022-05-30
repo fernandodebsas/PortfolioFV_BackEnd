26857d5c9f5f01c86b407b04c9dccb176bd3a3ec
fernandodebsas <fernandodebsas@hotmail.com>
2022-05-29 21:16:42 -0300

    cors bean removed

--- src/main/java/com/portfolio/fv/FvApplication.java
+++ src/main/java/com/portfolio/fv/FvApplication.java
@@ -1,15 +1,7 @@
 package com.portfolio.fv;
 
-import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class FvApplication {
@@ -17,15 +9,5 @@ public class FvApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FvApplication.class, args);
 	}
-        
-        @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
-    }
 
 }
