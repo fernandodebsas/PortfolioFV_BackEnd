8e7750ed8aecbc681094e9100942230eb0a296f1
fernandodebsas <fernandodebsas@hotmail.com>
2022-06-12 13:05:24 -0300

    updated cors and db

--- src/main/java/com/portfolio/fv/Controller/UsuarioController.java
+++ src/main/java/com/portfolio/fv/Controller/UsuarioController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliofv.herokuapp.com")
 @RequestMapping("/usuario")
 public class UsuarioController {
     private final UsuarioService usuarioService;
--- src/main/java/com/portfolio/fv/FvApplication.java
+++ src/main/java/com/portfolio/fv/FvApplication.java
@@ -14,7 +14,7 @@ public class FvApplication {
 //	public CorsFilter corsFilter() {
 //		CorsConfiguration corsConfiguration = new CorsConfiguration();
 //		corsConfiguration.setAllowCredentials(true);
-//		corsConfiguration.setAllowedOrigins(Arrays.asList("https://fv-portfolio.web.app"));
+//		corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfoliofv.herokuapp.com"));
 //		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 //				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 //				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
